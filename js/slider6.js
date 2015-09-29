372b378e477d48d10d4d6756f23af2a80c7be974
Shanshan Chen <shanshanc.chen@gmail.com>
2015-09-29 13:26:58 +0800

    update main js

--- js/slider6.js
+++ js/slider6.js
@@ -8,3 +8,26 @@ $(window).load(function() {
 
 
 });
+
+
+// sticky navigation
+$('#topNav').sticky({
+	responsiveWidth: true
+
+});
+
+
+// chart
+google.load('visualization', '1');   // Don't need to specify chart libraries!
+      google.setOnLoadCallback(drawVisualization);
+
+      function drawVisualization() {
+        var wrapper = new google.visualization.ChartWrapper({
+          chartType: 'ColumnChart',
+          dataTable: [['', 'Germany', 'USA', 'Brazil', 'Canada', 'France', 'RU'],
+                      ['', 700, 300, 400, 500, 600, 800]],
+          options: {'title': 'Countries'},
+          containerId: 'vis_div'
+        });
+        wrapper.draw();
+      }
